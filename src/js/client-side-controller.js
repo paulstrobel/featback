b43820899345667c221f068b682575492b0f1783
Paul Strobel <account@paulstrobel.de>
2017-04-01 00:12:33 +0200

    Delete client-side-controller.js

--- src/js/client-side-controller.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	$('#ddlMember').on('change',function(){
-	    var get=$('select option:selected').text();
-	    document.getElementById('text_area').value=get;
-	});
-}
